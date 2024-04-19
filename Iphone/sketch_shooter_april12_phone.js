3bdf4f6ffad343f8ec75caa047c2c658676cc300
camillegonnsen <millegonnsen@gmail.com>
2024-04-19 12:50:33 +0200

    Changed background

---
+++ Iphone/sketch_shooter_april12_phone.js
@@ -0,0 +1,34 @@
+let topic = "group17";
+let threshold = 0; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('red');
+  setupMQTT(topic);
+  
+}
+
+function onMessage(message) {
+  // empty by design
+}
+
+let alpha = 0.0;
+let beta = 0.0;
+
+function draw() {
+  if(orientationSensor.hasNewValue) {
+    let gyro = orientationSensor.get();
+    alpha = gyro.alpha;
+    beta = gyro.beta;
+  }
+}
+
+function touchStarted() {
+  setupOrientation(threshold);
+
+  sendMessage({
+    "from": config.myID,
+    "alpha": alpha,
+    "beta": beta
+  });
+}
